a251b724a511ae66fde74c1994f69cb4c561ae25
ankitkumar0008 <96786356+ankitkumar0008@users.noreply.github.com>
2025-09-25 10:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let container = document.querySelector(".container");
+
+for(let i = 1 ; i <=800;i++){
+	let square = document.createElement("div");
+	square.classList.add("square");
+	container.appendChild(square);
+}
