d72319a830947cd264ed5b5c5b226c9e8944af3d
ankitkumar0008 <96786356+ankitkumar0008@users.noreply.github.com>
2025-09-25 10:39:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //your JS code here. If required.
-let container = document.querySelector(".container");
 
-for(let i = 1 ; i <=800;i++){
-	let square = document.createElement("div");
-	square.classList.add("square");
+
+let container = document.querySelector('.container');
+for (let i = 0; i < 800; i++) {
+	let square = document.createElement('div');
+	square.classList.add('square');
+	square.addEventListener('mouseover',()=>{
+		square.style.backgroundColor = "greenyellow";
+		square.style.transition = "none";
+	})
+	square.addEventListener('mouseout',()=>{
+		square.style.backgroundColor = "rgb(29, 29, 29)";
+		square.style.transition = "background-color 1s ease";
+	})
 	container.appendChild(square);
 }
